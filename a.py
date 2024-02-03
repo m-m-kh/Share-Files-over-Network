ab3ef9dd77d903d6bce3f8f344af50f2e26347db
m-m-kh-py <khademim217@gmail.com>
2024-02-03 14:00:46 +0330

    V0.6

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ import psutil, os
 from urllib.parse import quote
 import socket
 import uvicorn
-
+import webbrowser
 
 host = socket.gethostbyname(socket.gethostname())
 app = FastAPI()
@@ -18,10 +18,10 @@ templates = Jinja2Templates(directory=os.path.dirname(__file__)+"/templates")
 
 @app.get("/{path:path}", response_class=HTMLResponse)
 async def read_root(request: Request, path = ""):
-    
-
+    context={"request":request}
     if not path:
         datas = [disk.device for disk in psutil.disk_partitions()]
+        context["datas"] = datas
     else:
         file = request.url.path[:-1]
         if file.startswith("/"):
@@ -29,16 +29,16 @@ async def read_root(request: Request, path = ""):
         if not os.path.isdir(file):
             print(os.path.basename(file))
             return FileResponse(file, filename=os.path.basename(file))
-            
-            
-        datas = os.listdir(path)
-        datas = [{"path":quote(p),"name":p} for p in datas]
         
+        datas = os.listdir(path)
+        datas = [p for p in datas]
+        back = "".join(list(map(lambda x : x+"/",str(request.url).split("/")))[:-2])
+        context["datas"] = datas
+        context["back"] = back
         
     return templates.TemplateResponse("index.html", 
-                                    context={"request":request,
-                                            "datas":datas})
+                                    context=context)
 
 
 if __name__=="__main__":
-    uvicorn.run(app=app)
+    uvicorn.run(app=app, host=host, port=4626)
